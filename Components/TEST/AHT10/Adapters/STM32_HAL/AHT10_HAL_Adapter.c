1bfa4385516765f45a0a4b4420620858ffbdb70e
Alex337-del <83550413+Alex337-del@users.noreply.github.com>
2024-02-07 16:50:22 +0300

    Code correction

--- Components/TEST/AHT10/Adapters/STM32_HAL/AHT10_HAL_Adapter.c
+++ Components/TEST/AHT10/Adapters/STM32_HAL/AHT10_HAL_Adapter.c
@@ -42,13 +42,15 @@ static xResult privateRequestListener(AHT10_T* driver,
 //------------------------------------------------------------------------------
 
 HAL_StatusTypeDef result;
+int state = 0;
+
 //------------------------------------------------------------------------------
 static void privateHandler(AHT10_T* driver)
 {
 	uint16_t AHT10_RX_Data[6];
 	uint32_t AHT10_ADC_Raw_1;
 	uint32_t AHT10_ADC_Raw_2;
-	uint8_t AHT10_TmpHum_Cmd = 0xAC;
+	uint8_t AHT10_TmpHum_Cmd[3] = {0xAC, 0x33, 0x00};
 
 
 	AHT10_HAL_AdapterT* adapter = driver->Adapter.Content;
@@ -57,23 +59,33 @@ static void privateHandler(AHT10_T* driver)
 	if (time - adapter->Content.TimeStamp > 1000)
 	{
 		adapter->Content.TimeStamp  = time;
-		HAL_I2C_Master_Transmit(&hi2c2, AHT10_ADDRESS, &AHT10_TmpHum_Cmd, 1, 150);
-		HAL_Delay(100); // Delay must be > 75 ms
-		result =  HAL_I2C_Master_Transmit(&hi2c2, AHT10_ADDRESS, &AHT10_TmpHum_Cmd, 1, 150);
+		result =  HAL_I2C_Master_Transmit(&hi2c2, AHT10_ADDRESS, AHT10_TmpHum_Cmd, 3, 150);
 
-		HAL_I2C_Master_Receive(&hi2c2, AHT10_ADDRESS, (uint8_t*)AHT10_RX_Data, 6, 150);
-		result = HAL_I2C_Master_Receive(&hi2c2, AHT10_ADDRESS, (uint8_t*)AHT10_RX_Data, 6, 150);
+		if(HAL_OK != result)
+		{
+			state = 1;
+		}
+		HAL_Delay(150);
+		result = HAL_I2C_Master_Receive(&hi2c2, AHT10_ADDRESS | 0x01, (uint8_t*)AHT10_RX_Data, 6, 150);
+
+		if(HAL_OK != result)
+		{
+			state = 2;
+		}
 
 		/* Convert to Humidity in °C */
-	    AHT10_ADC_Raw_1 = ((uint32_t)AHT10_RX_Data[1]  << 12) | ((uint16_t)AHT10_RX_Data[2] << 8) | AHT10_RX_Data[3] >> 4;
-	    adapter->Content.AHT10_Humidity = (float)((AHT10_ADC_Raw_1  / 1048576) * 100) ;
-
+		AHT10_ADC_Raw_1 = ((uint32_t)AHT10_RX_Data[1]  << 12) | ((uint16_t)AHT10_RX_Data[2] << 8) | AHT10_RX_Data[3] >> 4;
+		adapter->Content.AHT10_Humidity = (float)((AHT10_ADC_Raw_1  / 1048576) * 100) ;
+		HAL_Delay(80);
 		/* Convert to Temperature in °C */
-		AHT10_ADC_Raw_2 = (((uint32_t)AHT10_RX_Data[3] & 15) << 16 )| ((uint16_t)AHT10_RX_Data[4] << 8) | AHT10_RX_Data[5];
+		AHT10_ADC_Raw_2 = (((uint32_t)AHT10_RX_Data[3] & 0x0f) << 16 ) | ((uint16_t)AHT10_RX_Data[4] << 8) | AHT10_RX_Data[5];
 		adapter->Content.AHT10_Temperature = (float)(AHT10_ADC_Raw_2 * 200 / 1048576) - 50;
 		HAL_Delay(80);
-	}
+
+
+
 	//DigitalOutputsServiceAdapterT* adapter = service->Base.Adapter.Content;
+	}
 }
 //==============================================================================
 //initializations:
